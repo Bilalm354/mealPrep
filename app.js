fcc87761e760c4f3bece335f9088bfdb7479f563
Bilal <bilalm354@gmail.com>
2019-10-23 21:38:10 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var foodItems = {
+    chicken: {
+        quantity: 4,
+    },
+    rice: { 
+        quantity: 5,
+    }
+};
+
+function eat(){
+    
+}
+
+
+
+
+
+console.log('Number of boxes of chicken: ' + foodItems.chicken.quantity);
+console.log('Number of boxes of rice: ' + foodItems.rice.quantity);
+console.log('hi');
+console.log('hiya');
